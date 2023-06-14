1288d187f27f95b393d76cad27f438d471e36cb7
ricky6362 <63843083+ricky6362@users.noreply.github.com>
2023-06-14 13:08:27 +0300

    Some commit changes made

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Appending my cat name
+
+
+//Prepend cat name
+cats.unshift("Jerry");
+
+//Remove last cat
+cats.pop();
+
+//Remove first cat
+cats.shift();
